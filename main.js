36e59fdfeaf9d55a80f8582b22a73fbb57c78fbd
swodobaggins <swodobaggins@gmail.com>
2017-03-14 11:58:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var Main = function(game){
+  
+};
+
+Main.prototype = {
+  create: function(){
+    
+  },
+  
+  update: function(){
+    
+  },
+  
+  gameOver: function(){
+    this.game.state.start("GameOver")
+  }
+}
